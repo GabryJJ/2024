f9b5d874a0173fc260703dab132412f3c77cc926
Gabriele Tifni <gabrieletifni10@gmail.com>
2024-10-13 09:23:40 +0200

    esempiolist_changes

--- esempiolist.py
+++ esempiolist.py
@@ -1,6 +1,11 @@
+#stampo tutti gli elementi di una lista
 nome=['Gabriele','Tifni']
 for i in range(len(nome)):
     print(nome[i])
+#metodo alternativo per fare la stessa cosa:
+lucky=['star','13','jack']
+for s in lucky:
+    print(s)
 
 num=[1,2,3,4,42,24,22,5,323]
 for i,s in enumerate(num):
